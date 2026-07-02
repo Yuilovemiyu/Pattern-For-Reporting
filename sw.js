ad44d89ba7f282cb75b5f750aa74997874a07033
Yuilovemiyu <157918693+Yuilovemiyu@users.noreply.github.com>
2026-07-02 20:37:47 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "report-v2"; // change this when you want to force refresh
+const CACHE_NAME = "report-v3"; // change this when you want to force refresh
 
 const ASSETS = [
   "/",
